ce19db7a274bdf30ca58ec8ed4ed0ab41049cbea
myselfHimanshu <047himanshu@gmail.com>
2018-02-28 10:24:38 +0530

    rnn basic

---
+++ RNN/.ipynb_checkpoints/rnn1-checkpoint.py
@@ -0,0 +1,80 @@
+from __future__ import print_function, division
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+num_epochs = 100
+total_series_length = 50000
+truncated_backprop_length = 15
+state_size = 4
+num_classes = 2
+echo_step = 3
+batch_size = 5
+num_batches = total_series_length//truncated_backprop_length
+
+
+#Generate Data
+def generateData():
+    x = np.array(np.random.choice(2, total_series_length, p=[0.5,0.5]))
+    y = np.roll(x, echo_step)
+    y[0:echo_step] = 0
+    
+    x = x.reshape((batch_size, -1))
+    y = y.reshape((batch_size, -1))
+    
+    return (x,y)
+
+batchX_placeholder = tf.placeholder(tf.float32, [batch_size, truncated_backprop_length])
+batchY_placeholder = tf.placeholder(tf.float32, [batch_size, truncated_backprop_length])
+
+init_state = tf.placeholder(tf.float32, [batch_size, state_size])
+
+W = tf.Variable(np.random.rand(state_size+1, state_size), dtype=tf.float32)
+b = tf.Variable(np.zeros((1, state_size)), dtype=tf.float32)
+
+W2 = tf.Variable(np.random.rand(state_size, num_classes), dtype=tf.float32)
+b2 = tf.Variable(np.zeros((1, num_classes)), dtype=tf.float32)
+
+#unpack columns
+inputs_series = tf.unstack(batchX_placeholder, axis=1)
+labels_series = tf.unstack(batchY_placeholder, axis=1)
+
+#forward pass
+current_state = init_state
+states_series = []
+
+for current_input in inputs_series:
+    current_input = tf.reshape(current_input, [batch_size,1])
+    input_and_state_concatenated = tf.concat([current_input, current_state],1)
+    
+    next_state = tf.tanh(tf.matmul(input_and_state_concatenated,W)+b)
+    states_series.append(next_state)
+    current_state = next_state
+
+#Calculating loss
+logits_series = [tf.matmul(state,W2)+b2 ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
